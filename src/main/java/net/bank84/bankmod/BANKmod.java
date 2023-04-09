ad18b25e1898432975a1e25b4c10194228a1e8c4
chromosthete <maxfriday199@gmail.com>
2023-04-08 22:18:24 -0700

    lol here u go

---
+++ src/main/java/net/bank84/bankmod/BANKmod.java
@@ -0,0 +1,19 @@
+package net.bank84.bankmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BANKmod implements ModInitializer {
+	public static final String MOD_ID = "bankmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/bank84/bankmod/BANKmodClient.java
@@ -0,0 +1,10 @@
+package net.bank84.bankmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BANKmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
